45950a58ec10d8e5500b9c2d48837ae593fedb1d
shkim5164 <gk457@naver.com>
2016-07-16 20:28:48 +0000

    givemetheshit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
   def index
+    if current_user.couting.to_i >10
+      redirect_to '/home/kkk', notice: '못드감 시벌'
+  end
+
+
+
+
+
+
+
+
+
+
+
+  def kkk
+
+
+  end
+
+
+
+
+
+
+
+  def show
+    current_user.counting += 1
+    current_user.save
   end
 end
